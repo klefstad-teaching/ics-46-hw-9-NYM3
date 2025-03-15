3f233f845130848c31cf88559ce1bb875db23290
NYM3 <yimingn3@uci.edu>
2025-03-14 18:13:41 -0700

    Update dijkstras_main.cpp

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -21,10 +21,13 @@ int main(int argc, char* argv[]) {
         vector<int> path = extract_shortest_path(dist, previous, v);
 
         if (!path.empty()) {
-      for (int i = 0; i < (int)path.size(); i++) {
-    cout << path[i] << " ";
+for (int i = 0; i < (int)path.size(); i++) {
+    cout << path[i];
+    if (i < (int)path.size() - 1) {
+        cout << " ";
+    }
 }
-  cout << endl;
+cout << endl;
          cout << "Total cost is " << dist[v] << endl;
         } else {
             cout << v << " is unreachable from 0." << endl;
